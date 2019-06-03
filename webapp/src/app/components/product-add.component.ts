995a9b64d42930e49e0191e01c1278d10c04ac0a
kabalord <wroaserrano@gmail.com>
2019-06-03 20:43:25 +0200

    correccion ruta product-add.component.ts

--- webapp/src/app/components/product-add.component.ts
+++ webapp/src/app/components/product-add.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { ProductoService } from '../services/producto.service';
-import { Producto } from '..models/producto';
+import { Producto } from '../models/producto';
 
 @Component({
 	selector: 'product-add',
