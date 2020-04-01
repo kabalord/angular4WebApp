682e3201bf029d9310dfa7c4e8a269eeff9cabaf
kabalord <wroaserrano@gmail.com>
2020-04-01 12:58:43 +0200

    subida de imagenes, insertar url de la imagen a la bd, boton submit funcionando

--- webapp/src/app/components/product-add.component.ts
+++ webapp/src/app/components/product-add.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { ProductoService } from '../services/producto.service';
 import { Producto } from '../models/producto';
+import { GLOBAL } from '../services/global';
 
 @Component({
 	selector: 'product-add',
@@ -13,6 +14,8 @@ import { Producto } from '../models/producto';
 export class ProductAddComponent {
 	public titulo: string;
 	public producto: Producto;
+	public filesToUpload;
+	public resultUpload;
 
 	constructor(
 		private _productoService: ProductoService,
@@ -30,21 +33,41 @@ export class ProductAddComponent {
 	onSubmit(){
 		console.log(this.producto);
 
-		this._productoService.addProducto(this.producto).subscribe(
+		if(this.filesToUpload && this.filesToUpload.length >= 1){
+			this._productoService.makeFileRequest(GLOBAL.url+'upload-file', [], this.filesToUpload).then((result) => {
+				console.log(result);
 
-			response => {
-			if(response.code == 200){
-				this._router.navigate(['/products']);
+				this.resultUpload = result;
+				this.producto.imagen = this.resultUpload.filename;
+				this.saveProducto();
 
-				}else{
-					alert(response);
+			}, (error) =>{
+				console.log(error);
+			});
+		}else{
+			this.saveProducto();	
+		}
+
+	}
+
+	saveProducto(){
+			this._productoService.addProducto(this.producto).subscribe(
+				response => {
+					if(response.code == 200){
+						this._router.navigate(['/products']);
+					}else{
+						console.log(response);
+					}
+				},
+				error => {
+					console.log(<any>error);
 				}
-			},
-			error => {
-				alert(<any>error);
-			}
+			);
+	}
 
-		);
+	fileChangeEvent(fileInput: any){
+		this.filesToUpload = <Array<File>>fileInput.target.files;
+		console.log(this.filesToUpload);
 	}
 
 }
